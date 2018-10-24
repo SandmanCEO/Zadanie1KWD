afee0293d32566e154d4795594304ee9836e0188
SandmanCEO <grzegorzeczek@gmail.com>
2018-10-24 15:03:11 +0200

    Predict and score working now.

--- Main.py
+++ Main.py
@@ -1,7 +1,5 @@
 import pandas as pd
-import numpy as np
 import kNN
-import scipy.spatial.distance as sp
 
 colnames =["col1", "col2", "col3", "col4", "label"]
 dataLearning = pd.read_csv("iris.data.learning", names=colnames)
@@ -10,6 +8,7 @@ dataTest = pd.read_csv("iris.data.test", names=colnames)
 dataList = dataLearning.values.tolist()
 testList = dataTest.values.tolist()
 
-test = kNN.kNN(1, dataList)
+test = kNN.kNN(0.6, dataList)
 
-test.predict(testList)
+print(test.predict(testList))
+print(test.score(testList))
--- kNN.py
+++ kNN.py
@@ -1,26 +1,109 @@
 import scipy.spatial.distance as sp
 
+
 class kNN:
     def __init__(self, k, *data):
         self.k = k
         self.data = data
 
 
+
+
     def predict(self, *test):
 
         j = 0
+        dist = []
+        numberOfOcurrencies = {}
         result = []
 
         while(j < len(test[0])):
-            del result[:]
+            dist.clear()
+            numberOfOcurrencies.clear()
             i = 0
+            k=0
+            maxIndex = 0
+            max = 0
+            prediction = ''
+
             while(i < len(self.data[0])):
                 x = [[self.data[0][i][0], self.data[0][i][1], self.data[0][i][2], self.data[0][i][3]], [test[0][j][0], test[0][j][1], test[0][j][2], test[0][j][3]]]
                 y = sp.pdist(x, 'euclidean')
-                result.append([y, self.data[0][i][4]])
+                dist.append([y, self.data[0][i][4]])
                 i += 1
 
-            result.sort(key=lambda result:result[0])
+            dist.sort(key=lambda dist:dist[0])
+
+            while(dist[maxIndex][0] < self.k):
+                if dist[maxIndex][1] in numberOfOcurrencies:
+                    temporary = numberOfOcurrencies[dist[maxIndex][1]] + 1
+                    del numberOfOcurrencies[dist[maxIndex][1]]
+                    numberOfOcurrencies[dist[maxIndex][1]] = temporary
+                else:
+                    numberOfOcurrencies[dist[maxIndex][1]] = 1
+
+                maxIndex += 1
+
+            for item in numberOfOcurrencies:
+                if numberOfOcurrencies[item] > max:
+                    max = numberOfOcurrencies[item]
+                    prediction = item
+
+            result.append(prediction)
             j += 1
-            print(result)
+
+        return result
+
+    def score(self, *test):
+        j = 0
+        dist = []
+        numberOfOcurrencies = {}
+        result = []
+
+        while(j < len(test[0])):
+            dist.clear()
+            numberOfOcurrencies.clear()
+            i = 0
+            k=0
+            maxIndex = 0
+            max = 0
+            prediction = ''
+
+            while(i < len(self.data[0])):
+                x = [[self.data[0][i][0], self.data[0][i][1], self.data[0][i][2], self.data[0][i][3]], [test[0][j][0], test[0][j][1], test[0][j][2], test[0][j][3]]]
+                y = sp.pdist(x, 'euclidean')
+                dist.append([y, self.data[0][i][4]])
+                i += 1
+
+            dist.sort(key=lambda dist:dist[0])
+
+            while(dist[maxIndex][0] < self.k):
+                if dist[maxIndex][1] in numberOfOcurrencies:
+                    temporary = numberOfOcurrencies[dist[maxIndex][1]] + 1
+                    del numberOfOcurrencies[dist[maxIndex][1]]
+                    numberOfOcurrencies[dist[maxIndex][1]] = temporary
+                else:
+                    numberOfOcurrencies[dist[maxIndex][1]] = 1
+
+                maxIndex += 1
+
+            for item in numberOfOcurrencies:
+                if numberOfOcurrencies[item] > max:
+                    max = numberOfOcurrencies[item]
+                    prediction = item
+
+            result.append(prediction)
+            j += 1
+
+        i = 0
+        goodAnswers = 0
+        totalAnswers = 0
+
+        while i < len(test[0]):
+            if test[0][i][4] == result[i]:
+                totalAnswers += 1
+                goodAnswers += 1
+            else:
+                totalAnswers += 1
+            i += 1
+        return goodAnswers/totalAnswers
 
