358b119eceb561a78be636f0a363a24c27ecdc03
flisergio <sergey.dko@gmail.com>
2018-10-28 23:24:05 +0100

    Update UnitTesting.py

--- UnitTesting.py
+++ UnitTesting.py
@@ -1,24 +1,17 @@
 import pandas as pd
 import kNN
 import unittest
-
-colnames =["col1", "col2", "col3", "col4", "label"]
-dataLearning = pd.read_csv("iris.data.learning", names=colnames)
-dataTest = pd.read_csv("iris.data.test", names=colnames)
-
-dataList = dataLearning.values.tolist()
-testList = dataTest.values.tolist()
-
-test = kNN.kNN(123, dataList)
+import Main
 
 class TestkNN(unittest.TestCase):
 
     def test_predict(self):
-        self.assertIsNotNone(test.predict(testList))
+        self.assertIsNotNone(Main.test.predict(Main.testList))
 
     def test_score(self):
-        self.assertGreaterEqual(test.score(testList), 0)
-        self.assertLessEqual(test.score(testList), 1)
-
-"""if __name__ == '__main__':
-    unittest.main()"""
+        self.assertGreaterEqual(Main.test.score(Main.testList), 0)
+        self.assertLessEqual(Main.test.score(Main.testList), 1)
+"""
+if __name__ == '__main__':
+    unittest.main()
+"""
