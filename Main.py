c30cafeb2f4a0c1bfcf64201e734e55e315c62be
flisergio <sergey.dko@gmail.com>
2018-10-28 23:20:17 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import kNN
+import unittest
+import UnitTesting
 
 colnames =["col1", "col2", "col3", "col4", "label"]
 dataLearning = pd.read_csv("iris.data.learning", names=colnames)
@@ -8,7 +10,14 @@ dataTest = pd.read_csv("iris.data.test", names=colnames)
 dataList = dataLearning.values.tolist()
 testList = dataTest.values.tolist()
 
-test = kNN.kNN(123, dataList)
+test = kNN.kNN(121, dataList)
 
 print(test.predict(testList))
 print(test.score(testList))
+
+""" DALEJ JEST ZMIENIONE """
+
+UnitTesting.TestkNN.test_predict(unittest.TestCase)
+
+if __name__ == '__main__':
+    unittest.main()
